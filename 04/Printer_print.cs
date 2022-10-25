46cbaaa2b533273fb0d22ded65f5a61a55d627e6
MyHappySun <112709363+deprEsS-Today@users.noreply.github.com>
2022-10-25 13:37:03 +0300

    comm2
    printing completed

---
+++ 04/Printer_print.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_5
+{
+    public class Printer
+    {
+        public void IAmPrinting(Object someobj)
+        {
+            Console.WriteLine(someobj.GetType());
+            Console.WriteLine(someobj.ToString());
+        }
+    }
+}
